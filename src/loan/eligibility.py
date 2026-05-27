f82d807f86e2afbb11c17f7a03b23ae84cf49605
ZahidDT <zahid.diaz@jelou.ai>
2026-05-27 14:47:42 -0500

    pylint(C0116): add missing docstring for member classification function

--- src/loan/eligibility.py
+++ src/loan/eligibility.py
@@ -161,6 +161,9 @@ def format_report(result, member_name):
 
 
 def get_audit_count():
+
+    """Returns the current value of the audit counter, which tracks how many times the evaluate function has been called."""
+
     return AUDIT_COUNTER[0]
 
 
